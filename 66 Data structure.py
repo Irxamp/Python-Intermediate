1ee0d09f9a7a92ffcc8a554a7db0ce60ae97e4a7
Irxamp <88538593+Irxamp@users.noreply.github.com>
2024-02-05 20:25:31 +0200

    Add files via upload

---
+++ 66 Data structure.py
@@ -0,0 +1,10 @@
+Varstack=["John","Reema","Xeenat"]
+Varstack.append("Jolly")
+Varstack.append("Leena")
+print(Varstack)
+print(Varstack.pop())
+print(Varstack)
+print(Varstack.pop())
+print(Varstack)
+
+ 
---
+++ 67 Data structure.py
@@ -0,0 +1,8 @@
+Varqueue=["Leena","meena","Zeenat"]
+Varqueue.append("John")
+Varqueue.append("Jolly")
+print(Varqueue)
+print(Varqueue.pop(0))
+print(Varqueue)
+print(Varqueue.pop(0))
+ 
